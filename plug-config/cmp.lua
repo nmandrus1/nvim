7c20c3237a4a96e2787b51aa804035e2d3cdc5f6
Niels <nmandrus1@gmail.com>
2021-11-18 16:31:26 -0500

    fixed tabbing

--- plug-config/cmp.lua
+++ plug-config/cmp.lua
@@ -27,8 +27,9 @@ cmp.setup({
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      -- Dont know what this does but its breaking the ability to Tab
+      -- elseif luasnip.expand_or_jumpable() then
+      --   luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -36,8 +37,9 @@ cmp.setup({
     ['<S-Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+      -- Dont know what this does but its breaking the ability to Tab
+      -- elseif luasnip.jumpable(-1) then
+      --   luasnip.jump(-1)
       else
         fallback()
       end
